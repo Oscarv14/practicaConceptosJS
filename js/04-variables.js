3c123b6540767f8c3cbac421aedace7168498f18
Oscar Ventura <oventura115@gmail.com>
2024-07-03 00:13:53 -0300

    Se hizo los primeros 5 ejercicios y se creo el archivo de otros 5 ejercicios

---
+++ js/04-variables.js
@@ -0,0 +1,2 @@
+const nombre1 = prompt("Escriba el nombre de usuario");
+document.write("Hola " + nombre1);
---
+++ js/05-variables.js
@@ -0,0 +1,3 @@
+const numero1 = parseInt(prompt("Escriba un número"));
+const numero2 = parseInt(prompt("Escriba otro número"));
+document.write("El resutado de sumar estos 2 números es: " + (numero1 + numero2));
